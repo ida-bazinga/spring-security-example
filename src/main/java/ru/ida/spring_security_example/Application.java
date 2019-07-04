db39bbac4bc326df079f3ceb27c7822235c89bd6
EXPOFORUM\d.ivanov <d.ivavov@expoforum.ru>
2019-07-04 09:48:28 +0300

    example
    example

---
+++ src/main/java/ru/ida/spring_security_example/Application.java
@@ -0,0 +1,13 @@
+package ru.ida.spring_security_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/ru/ida/spring_security_example/MvcConfig.java
@@ -0,0 +1,18 @@
+package ru.ida.spring_security_example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/base").setViewName("base");
+        registry.addViewController("/").setViewName("base");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
